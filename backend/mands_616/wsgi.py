24627acd3fe61684d4b6dd7aee168b226c7f1c21
Crowdbotics <engineering@crowdbotics.com>
2024-08-29 09:49:30 +0000

    Initial commit

---
+++ backend/mands_616/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for mands_616 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "mands_616.settings"
+)
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "mands-616",
+  url: "https://mands-616.botics.co",
+  api: "https://mands-616.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
